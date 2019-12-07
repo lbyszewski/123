a0fccd8e269beeee95eb9469e7c6ed689ebb2ea4
Lukasz <lukasz.byszewski@gmail.com>
2019-12-07 20:37:41 +0100

    123

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,3 +1,6 @@
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
 public class Kalkulator {
     public static int  dodaj(int ... paramtry) {
         int wynik = 0;
@@ -6,6 +9,30 @@ public class Kalkulator {
 
         }
         return wynik;
+
     }
 
+    public static void main(String[] args) {
+        System.out.println("początek zakresu");
+        Scanner skaner = new Scanner(System.in);
+
+        int poczatekZakresu =  skaner.nextInt();
+
+        System.out.println("koniec zakresu");
+        double koniecZakresu =  skaner.nextDouble();
+
+        if (poczatekZakresu<koniecZakresu)
+        {
+            System.out.println("dzielnik: ");
+            int dzielnik = skaner.nextInt();
+
+            for (int i = poczatekZakresu ; i<koniecZakresu;i++)
+            {
+                if(i % dzielnik == 0 && i % dzielnik == 0)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
 }
